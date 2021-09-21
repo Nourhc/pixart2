d9ae1580802de5b6376c6966363f8f724bb2c38c
main.py <nourhc>
2021-09-21 15:01:18 +0400

    10% done

---
+++ pixart.py
@@ -0,0 +1,27 @@
+
+
+import turtle
+
+def XMIN(param):
+     pass
+
+
+
+XMax = 200
+YMAX = -XMax
+XMax = 200
+YMax = -XMax
+XMax = 200
+
+
+ 
+
+def init(YMAX=None):
+    turtle.reset()
+    turtle.goto(XMax , YMAX)
+
+    turle.speed(0)
+
+    S= turtle.speed()
+    assert(S==0)
+
---
+++ pixart_test.py
@@ -0,0 +1,11 @@
+import pixart_test
+import turtle
+
+
+def test_init
+    pixart_test.init()
+    s = turtle.speed()
+    assert (x == 0)
+
+    x = turtle.xcor():
+    assert (x == -200)
